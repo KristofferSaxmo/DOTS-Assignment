d2ae6d7d910b1835d9b057176ab9aeb598868229
KristofferSaxmo <ks02@hotmail.se>
2023-11-03 18:54:07 +0100

    Cleaned up collision code

--- Assets/Scripts/CollisionSystem.cs
+++ Assets/Scripts/CollisionSystem.cs
@@ -9,23 +9,19 @@ using Unity.Transforms;
 public partial struct CollisionSystem : ISystem
 {
     private const float Precision = 0.2f;
+    const float PrecisionSquared = Precision * Precision;
+    
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<Config>();
         state.RequireForUpdate<Asteroid>();
         state.RequireForUpdate<Bullet>();
-        state.RequireForUpdate<Player>();
     }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-       
-        Config config = SystemAPI.GetSingleton<Config>();
-        var deltaTime = SystemAPI.Time.DeltaTime;
-        Player player = SystemAPI.GetSingleton<Player>();
-        
         foreach (var (asteroidTransform, asteroidEntity) in SystemAPI.Query<RefRW<LocalTransform>>().WithAll<Asteroid>()
                      .WithEntityAccess())
         {
@@ -44,8 +40,8 @@ public partial struct CollisionSystem : ISystem
     private bool CheckCollision(float3 position1, float3 position2)
     {
         float3 delta = position1 - position2;
-        float distanceSquared = math.dot(delta, delta); // Squared distance for performance.
-        const float precisionSquared = Precision * Precision;
-        return distanceSquared < precisionSquared;
+        float distanceSquared = math.dot(delta, delta);
+        
+        return distanceSquared < PrecisionSquared;
     }
 }
